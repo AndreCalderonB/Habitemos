0e6690e0ba2382c832568769e2efb861a839c5b7
Andre <andre.calder98@gmail.com>
2022-06-14 10:34:20 -0500

    Visual updates

--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "./logo";
 function Entry() {
   return (
-    <div className="screen flex flex-center">
+    <div className="screen screenone flex flex-center">
       <div className="container">
         <Logo />
         <h2 className="raleway">Juguemos a conversar</h2>
